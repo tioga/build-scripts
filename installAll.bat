70e475e0cdd0bd897e587d594f846893ac6e40a3
Jacob D. Parr <me@jacobparr.com>
2015-04-23 16:04:50 -0700

    Updated build scripts to force a clean and build.

--- installAll.bat
+++ installAll.bat
@@ -2,47 +2,47 @@
 cls
 
 echo Installing dev...
-call gradle -q -p ../dev install
+call gradle -q -p ../dev clean build install
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Installing CouchAce...
-call gradle -q -p ../CouchAce install
+call gradle -q -p ../CouchAce clean build install
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Installing runners...
-call gradle -q -p ../runners install
+call gradle -q -p ../runners clean build install
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Installing lib...
-call gradle -q -p ../lib install
+call gradle -q -p ../lib clean build install
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Installing apis...
-call gradle -q -p ../apis install
+call gradle -q -p ../apis clean build install
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Installing app...
-call gradle -q -p ../app install
+call gradle -q -p ../app clean build install
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Installing Push...
-call gradle -q -p ../Push install
+call gradle -q -p ../Push clean build install
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Installing Notify...
-call gradle -q -p ../Notify install
+call gradle -q -p ../Notify clean build install
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Installing Jobs...
-call gradle -q -p ../Jobs install
+call gradle -q -p ../Jobs clean build install
 echo.
 if errorlevel 1 ( GOTO:eof )
 
--- uploadArchives.bat
+++ uploadArchives.bat
@@ -2,47 +2,47 @@
 cls
 
 echo Uploading archives for dev...
-call gradle -Dno-fail-version-conflict=true -q -p ../dev uploadArchives
+call gradle -Dno-fail-version-conflict=true -q -p ../dev clean build uploadArchives
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Uploading archives for CouchAce...
-call gradle -Dno-fail-version-conflict=true -q -p ../CouchAce uploadArchives
+call gradle -Dno-fail-version-conflict=true -q -p ../CouchAce clean build uploadArchives
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Uploading archives for runners...
-call gradle -Dno-fail-version-conflict=true -q -p ../runners uploadArchives
+call gradle -Dno-fail-version-conflict=true -q -p ../runners clean build uploadArchives
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Uploading archives for lib...
-call gradle -Dno-fail-version-conflict=true -q -p ../lib uploadArchives
+call gradle -Dno-fail-version-conflict=true -q -p ../lib clean build uploadArchives
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Uploading archives for apis...
-call gradle -Dno-fail-version-conflict=true -q -p ../apis uploadArchives
+call gradle -Dno-fail-version-conflict=true -q -p ../apis clean build uploadArchives
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Uploading archives for app...
-call gradle -Dno-fail-version-conflict=true -q -p ../app uploadArchives
+call gradle -Dno-fail-version-conflict=true -q -p ../app clean build uploadArchives
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Uploading archives for Push...
-call gradle -Dno-fail-version-conflict=true -q -p ../Push uploadArchives
+call gradle -Dno-fail-version-conflict=true -q -p ../Push clean build uploadArchives
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Uploading archives for Notify...
-call gradle -Dno-fail-version-conflict=true -q -p ../Notify uploadArchives
+call gradle -Dno-fail-version-conflict=true -q -p ../Notify clean build uploadArchives
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Uploading archives for Jobs...
-call gradle -Dno-fail-version-conflict=true -q -p ../Jobs uploadArchives
+call gradle -Dno-fail-version-conflict=true -q -p ../Jobs clean build uploadArchives
 echo.
 if errorlevel 1 ( GOTO:eof )
 
