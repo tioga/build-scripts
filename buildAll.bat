d02e6b65114bec0b94b04599d530045ce782d108
Jacob D. Parr <me@jacobparr.com>
2015-05-04 09:25:41 -0700

    Updated script to clean as part of build.

--- buildAll.bat
+++ buildAll.bat
@@ -2,47 +2,47 @@
 cls
 
 echo Building dev...
-call gradle -q -p ../dev build
+call gradle -q -p ../dev clean build
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Building CouchAce...
-call gradle -q -p ../CouchAce build
+call gradle -q -p ../CouchAce clean build
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Building runners...
-call gradle -q -p ../runners build
+call gradle -q -p ../runners clean build
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Building lib...
-call gradle -q -p ../lib build
+call gradle -q -p ../lib clean build
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Building apis...
-call gradle -q -p ../apis build
+call gradle -q -p ../apis clean build
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Building app...
-call gradle -q -p ../app build
+call gradle -q -p ../app clean build
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Building Push...
-call gradle -q -p ../Push build
+call gradle -q -p ../Push clean build
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Building Notify...
-call gradle -q -p ../Notify build
+call gradle -q -p ../Notify clean build
 echo.
 if errorlevel 1 ( GOTO:eof )
 
 echo Building Jobs...
-call gradle -q -p ../Jobs build
+call gradle -q -p ../Jobs clean build
 echo.
 if errorlevel 1 ( GOTO:eof )
 
