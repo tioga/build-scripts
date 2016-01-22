ca4b14eb630f424d137ba5017f4232539969fe90
Harlan <harlan.work@gmail.com>
2016-01-21 16:59:09 -0800

    Added build scripts for lx/osx

---
+++ cleanAll.sh
@@ -0,0 +1,10 @@
+gradle -Dno-fail-version-conflict=true -p ../dev clean
+gradle -Dno-fail-version-conflict=true -p ../CouchAce clean
+gradle -Dno-fail-version-conflict=true -p ../runners clean
+gradle -Dno-fail-version-conflict=true -p ../lib clean
+gradle -Dno-fail-version-conflict=true -p ../apis clean
+gradle -Dno-fail-version-conflict=true -p ../app clean
+gradle -Dno-fail-version-conflict=true -p ../Push clean
+gradle -Dno-fail-version-conflict=true -p ../Notify clean
+gradle -Dno-fail-version-conflict=true -p ../Jobs clean
+gradle -Dno-fail-version-conflict=true -p ../runners clean
